f8f309a3053cebc6bf6960703cb15fa1b0056a14
Damien Cauquil <damien.cauquil@digitalsecurity.fr>
2016-11-17 19:25:11 +0100

    Same handle values option now by default (after months of testing)

--- resources/js/managers/interceptor.js
+++ resources/js/managers/interceptor.js
@@ -24,7 +24,7 @@ var Interceptor = function(){
 
   /* Options. */
   this.shouldReconnect = true;
-  this.keepHandles = false;
+  this.keepHandles = true;
 
   /* Registered hooks. */
   this.hooks = {};
